1a40160596e8ec91d4b3e35f88aa44203ac6cff3
sksoumik <sadmanks@gmail.com>
2019-02-18 19:33:23 +0600

    forum

---
+++ pcw/urls.py
@@ -0,0 +1,8 @@
+from machina.app import board
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('forum/', include(board.urls)),
+]
